1e3b3f8267c6acb3329f8db91fb430893ec6ccab
Caleb Colangelo <calebc041406@gmail.com>
2025-01-19 12:23:43 -0500

    Finished project!

---
+++ README.md
@@ -0,0 +1,25 @@
+# Professional README.MD Generator
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+I created this generator so that developers would be able to spend more time working on their actual project, and less time worrying about having a professional README. This application greatly reduces the time it takes to create a README, so devs dont have to worry about taking time out of their project to make one, just answer a few simple question and the work is done for you! It is all done using  node.js and inqurier. These two tools are what allowed me to create this app. 
+
+## Installation
+
+Install from the GitHub repo, and simply open index.js in the terminal and run the command "node index.js".
+
+## Usage
+
+Follow this video!
+    
+https://drive.google.com/file/d/1XuK0bVxzwNUm27W2Z2xKl74gUMIuI0f_/view
+
+## How to Contribute
+
+Email me or comment on my GitHub page!
+
+My email: calebc041406@gmail.com  
+
+My GitHub: https://github.com/CalebColangelo
--- index.js
+++ index.js
@@ -1,43 +1,7 @@
 import inquirer from 'inquirer';
 import fs from 'fs'
 
-
-const generateREADME = ({ projectName, shortDescription, installSteps, usageInfo, licenseChoice, contributeTo, contactEmail, githubUser }) => 
-`${projectName}
-## Description
-
-${shortDescription}
-
-## Table of Contents
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-${installSteps}
-
-## Usage
-
-${usageInfo}
-
-
-## License
-
-${licenseChoice}
-
-## Badges
-
-
-## How to Contribute
-
-${contributeTo}
-Email me at: ${contactEmail}
-or see my GitHub at ${githubUser} `
-
-const questionInputs = inquirer.prompt([
+inquirer.prompt([
     {
         type: 'input',
         message: 'What is the name of your project?',
@@ -82,20 +46,60 @@ const questionInputs = inquirer.prompt([
         type: 'list',
         message: 'Which license would you like to use?',
         name: 'licenseChoice',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3.0', 'NONE']
+        choices: ['MIT', 'APACHE--2.0', 'GPL--3.0', 'BSD--3.0']
     }
 ])
     .then((response) => {
         const READMEcontent = generateREADME(response)
 
-        fs.writeFile('README.md', READMEcontent, (err) =>
-            err ? console.log(err) : console.log('Successfully created README.md!')
-          );
+        if (response.licenseChoice === 'MIT') {
+            let badgeDisplay = '![Static Badge](https://img.shields.io/badge/MIT_License-green)'
+        }
+        
+            fs.writeFile('GeneratedREADME.md', READMEcontent, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md!')
+            );
     })
 
-// TODO: Create a function to initialize app
-// function init() { }
+const generateREADME = ({ projectName, shortDescription, installSteps, usageInfo, licenseChoice, contributeTo, contactEmail, githubUser }) =>
+`![Static Badge](https://img.shields.io/badge/${licenseChoice}-blue)  
 
-// Function call to initialize app
-// init();
+${projectName}
 
+## Description
+
+${shortDescription}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contact](#Contact)
+
+## Installation
+
+${installSteps}
+
+## Usage
+
+${usageInfo}
+
+
+## License
+
+This project is licensed under the ${licenseChoice} license.  
+
+https://www.google.com/search?q=${licenseChoice}
+
+## Badges
+
+![Static Badge](https://img.shields.io/badge/${licenseChoice}-blue)
+
+## How to Contribute
+
+${contributeTo}
+
+## Contact 
+
+Email me at: ${contactEmail}  or see my GitHub at https://github.com/${githubUser} `
